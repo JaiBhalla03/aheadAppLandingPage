26766a51b26f1ab16a79d968fd9443e1304430f2
jai <jai.bhalla2021@vitstudent.ac.in>
2023-10-24 17:45:00 +0530

    adding the navbar component and makinng responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,64 @@
-import React from 'react';
+'use client';
+import React, {useEffect, useState} from 'react';
 import Image from "next/image";
 import logo from '../public/images/ahead-logo.png'
+import {Menu} from "lucide-react";
+import {Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger, SheetClose} from "@/components/ui/sheet";
+
 
 const Navbar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(()=>{
+        setIsMounted(true);
+    }, []);
+    if(!isMounted){
+        return null;
+    }
     return (
-        <nav className={'px-2 md:px-10 flex w-full items-center'}>
-            <div>
+        <nav className={'py-2 md:py-3 px-2 md:px-10 flex w-full justify-between items-center'}>
+            <div className={'h-10 md:h-12 w-10 md:w-12'}>
                 <Image src={logo} alt={''}/>
             </div>
-            <div>
-
+            <ul className={'hidden md:flex items-center gap-2 sm:gap-4 lg:gap-8 font-semibold text-gray-700 text-sm'}>
+               <li>Emotions</li>
+               <li>Manifesto</li>
+                <li>Self-awareness test</li>
+                <li>Work With Us</li>
+            </ul>
+            <div className={'hidden md:flex items-center justify-center'}>
+                <button className={'bg-black text-gray-100 rounded-full px-6 py-3 text-sm font-semibold'}>
+                    Download app
+                </button>
             </div>
-            <div>
+            <div className={'block md:hidden text-black'}>
+                <Sheet>
+                    <SheetTrigger>
+                        <button
+                            className={'md:hidden'}
 
+                        >
+                            <Menu className={'h-8 w-8 text-gray-800'}/>
+                        </button>
+                    </SheetTrigger>
+                    <SheetContent side={'left'} className={''}>
+                        <div className={'h-10 w-10'}>
+                            <Image src={logo} alt={''}/>
+                        </div>
+                        <div className={'pt-20 flex flex-col gap-8'}>
+                            <ul className={'text-gray-700 flex flex-col gap-8'}>
+                                <li>Emotions</li>
+                                <li>Manifesto</li>
+                                <li>Self-awareness test</li>
+                                <li>Work With Us</li>
+                            </ul>
+                            <div className={'flex items-center'}>
+                                <button className={'bg-black text-gray-100 rounded-full px-6 py-3 text-sm font-semibold'}>
+                                    Download app
+                                </button>
+                            </div>
+                        </div>
+                    </SheetContent>
+                </Sheet>
             </div>
         </nav>
     );
